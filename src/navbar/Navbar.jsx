4e004ce6a63bcbd63b85dd927643cfcf43dc38e3
AgusCorcico <corcicoagustin@gmail.com>
2023-02-07 18:06:30 -0300

    configuraciones y navbar

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import './navbar.scss'
+import SearchIcon from '@mui/icons-material/Search';
+import Notifications from '@mui/icons-material/NotificationsNone';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { useState } from 'react';
+
+function Navbar() {
+
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    //The onscroll event occurs when an element's scrollbar is being scrolled.
+    //Esta funcion es para que la navbar sea negra cuando el pageYOffset = 0 y transparente
+    //a medida que scrolleo para abajo
+    window.onscroll = () =>{
+        setIsScrolled(window.pageYOffset === 0 ? false : true)
+        return () => (window.onscroll = null);
+    }
+    console.log(setIsScrolled)
+    return (
+    <div className={ isScrolled ? "navbar scrolled" : "navbar"}>
+        <div className="container">
+            <div className="left">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+                    alt=""
+                />
+                <span>Inicio</span>
+                <span>Series</span>
+                <span>Películas</span>
+                <span>Novedades populares</span>
+                <span>Mi lista</span>
+                <span>Explora por idiomas</span>
+            </div>
+            <div className="right">
+                <SearchIcon className='icon'/>
+                <span>Niños</span>
+                <Notifications className='icon'/>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                    alt=""
+                />
+                <div className="profile">
+                    <ArrowDropDownIcon className='icon'/>
+                    <div className="options">
+                        <span>Administrar perfiles</span>
+                        <span>Transferir perfil</span>
+                        <span>Cuenta</span>
+                        <span>Centro de ayuda</span>
+                        <span>Cerrar sesión en Netflix</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+)};
+
+export default Navbar;
