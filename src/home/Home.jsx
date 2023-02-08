91fbcdaba7bf2fd5b0bf5b74ba756f50bbe7c26b
AgusCorcico <corcicoagustin@gmail.com>
2023-02-07 23:35:58 -0300

    create featured

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from '../featured/Featured';
 import Navbar from '../navbar/Navbar';
 import './home.scss'
 
@@ -5,39 +6,7 @@ function Home() {
     return (
     <div className="home">
         <Navbar />
-        <img
-        width="100%" 
-        src="https://i.eurosport.com/2022/12/19/3511011-71567388-2560-1440.jpg"
-        alt="" />
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque doloribus dolorum illum illo numquam eum officiis autem libero ipsa maxime, aspernatur consequuntur sit, odio soluta, consectetur dolorem dicta eius animi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui laborum, quia soluta voluptatum dicta voluptate! Minus unde quia, voluptate aut explicabo maiores eaque ratione reprehenderit laboriosam adipisci quasi corrupti?
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni ducimus deserunt nostrum porro cum odio facilis quae aspernatur aperiam omnis dignissimos consequatur possimus corrupti eos natus sapiente, non ab in.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, amet asperiores nostrum molestias minima itaque numquam, blanditiis quis magni, quo earum fugiat nemo non hic aperiam totam dolor fugit molestiae.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque doloribus dolorum illum illo numquam eum officiis autem libero ipsa maxime, aspernatur consequuntur sit, odio soluta, consectetur dolorem dicta eius animi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui laborum, quia soluta voluptatum dicta voluptate! Minus unde quia, voluptate aut explicabo maiores eaque ratione reprehenderit laboriosam adipisci quasi corrupti?
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni ducimus deserunt nostrum porro cum odio facilis quae aspernatur aperiam omnis dignissimos consequatur possimus corrupti eos natus sapiente, non ab in.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, amet asperiores nostrum molestias minima itaque numquam, blanditiis quis magni, quo earum fugiat nemo non hic aperiam totam dolor fugit molestiae.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque doloribus dolorum illum illo numquam eum officiis autem libero ipsa maxime, aspernatur consequuntur sit, odio soluta, consectetur dolorem dicta eius animi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui laborum, quia soluta voluptatum dicta voluptate! Minus unde quia, voluptate aut explicabo maiores eaque ratione reprehenderit laboriosam adipisci quasi corrupti?
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni ducimus deserunt nostrum porro cum odio facilis quae aspernatur aperiam omnis dignissimos consequatur possimus corrupti eos natus sapiente, non ab in.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, amet asperiores nostrum molestias minima itaque numquam, blanditiis quis magni, quo earum fugiat nemo non hic aperiam totam dolor fugit molestiae.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque doloribus dolorum illum illo numquam eum officiis autem libero ipsa maxime, aspernatur consequuntur sit, odio soluta, consectetur dolorem dicta eius animi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui laborum, quia soluta voluptatum dicta voluptate! Minus unde quia, voluptate aut explicabo maiores eaque ratione reprehenderit laboriosam adipisci quasi corrupti?
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni ducimus deserunt nostrum porro cum odio facilis quae aspernatur aperiam omnis dignissimos consequatur possimus corrupti eos natus sapiente, non ab in.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, amet asperiores nostrum molestias minima itaque numquam, blanditiis quis magni, quo earum fugiat nemo non hic aperiam totam dolor fugit molestiae.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque doloribus dolorum illum illo numquam eum officiis autem libero ipsa maxime, aspernatur consequuntur sit, odio soluta, consectetur dolorem dicta eius animi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui laborum, quia soluta voluptatum dicta voluptate! Minus unde quia, voluptate aut explicabo maiores eaque ratione reprehenderit laboriosam adipisci quasi corrupti?
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni ducimus deserunt nostrum porro cum odio facilis quae aspernatur aperiam omnis dignissimos consequatur possimus corrupti eos natus sapiente, non ab in.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, amet asperiores nostrum molestias minima itaque numquam, blanditiis quis magni, quo earum fugiat nemo non hic aperiam totam dolor fugit molestiae.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque doloribus dolorum illum illo numquam eum officiis autem libero ipsa maxime, aspernatur consequuntur sit, odio soluta, consectetur dolorem dicta eius animi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui laborum, quia soluta voluptatum dicta voluptate! Minus unde quia, voluptate aut explicabo maiores eaque ratione reprehenderit laboriosam adipisci quasi corrupti?
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni ducimus deserunt nostrum porro cum odio facilis quae aspernatur aperiam omnis dignissimos consequatur possimus corrupti eos natus sapiente, non ab in.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, amet asperiores nostrum molestias minima itaque numquam, blanditiis quis magni, quo earum fugiat nemo non hic aperiam totam dolor fugit molestiae.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque doloribus dolorum illum illo numquam eum officiis autem libero ipsa maxime, aspernatur consequuntur sit, odio soluta, consectetur dolorem dicta eius animi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui laborum, quia soluta voluptatum dicta voluptate! Minus unde quia, voluptate aut explicabo maiores eaque ratione reprehenderit laboriosam adipisci quasi corrupti?
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni ducimus deserunt nostrum porro cum odio facilis quae aspernatur aperiam omnis dignissimos consequatur possimus corrupti eos natus sapiente, non ab in.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, amet asperiores nostrum molestias minima itaque numquam, blanditiis quis magni, quo earum fugiat nemo non hic aperiam totam dolor fugit molestiae.
-
+        <Featured type='Películas'/>
     </div>
 )};
 
